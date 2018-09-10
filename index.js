ac135442c439b57a2b624bd3e1077c2363fa04af
Harold Alcala <harold.alcala@vcube.co.jp>
2018-09-10 15:13:04 +0800

    Throttle sending to 10 per minute

--- index.js
+++ index.js
@@ -87,6 +87,10 @@ exports.configure = function(__configuration) {
 	}
 };
 
+var channel_last_sent = exports.channel_last_sent = {};
+var channel_log_start = exports.channel_log_start = {};
+var channel_log_count = exports.channel_log_count = {};
+
 exports.send = function (message, options, channel) {
 	console.log("options", options);
 
@@ -109,6 +113,17 @@ exports.send = function (message, options, channel) {
 		throw "No such channel '" + channel + "'";
 	}
 
+	if (!channel_log_start[channel] || (new Date() - channel_log_start[channel]) > 60000) {
+		channel_log_count[channel] = 0;
+		channel_log_start[channel] = new Date();
+	}
+
+	channel_log_count[channel]++;
+
+	if (channel_log_count[channel] > 10 && ((new Date() - channel_log_start[channel]) < 60000)) {
+		return console.log("Error: alert-sender.send was called more than 10 times in a minute");
+	}
+
 	if (options.subject && options.subject.length > 100) {
 		options.subject = options.subject.substr(0, 100);
 	}
@@ -122,7 +137,7 @@ exports.send = function (message, options, channel) {
 
 		sns.publish(
 			sns_params,
-			function(err,data) {
+			function(err, data) {
 				if (err) {
 					console.log("Error sending a message "+err);
 				}
